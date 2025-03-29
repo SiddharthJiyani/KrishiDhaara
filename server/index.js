dc5ffadab7b8a3545087eec69fca6f93bc225748
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2025-03-29 12:40:17 +0530

    general routes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ import cors from 'cors';
 import connectDB from './db/db.js';
 import dotenv from "dotenv"
 // import temperatureRoute from "./routes/temperatureRoute.js"
-// import humidityRoute from "./routes/humidityRoute.js"
+import humidityRoute from "./routes/humidityRoute.js"
 // import relayRouter from "./routes/relayRoute.js"
 import authRouter from "./routes/authRoute.js"
 // import generalRouter from "./routes/generalRoute.js"
-// import plantRouter from "./routes/plantRoute.js"
+import plantRouter from "./routes/plantRoute.js"
 import scrapeRouter from "./routes/scrapeRoute.js"
 import reportRouter from "./routes/reportRoute.js"
 dotenv.config()
@@ -30,11 +30,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 
 // app.use('/SensorData/temperature',temperatureRoute)
-// app.use('/SensorData/humidity',humidityRoute)
+app.use('/SensorData/humidity',humidityRoute)
 // app.use('/SensorData/relay',relayRouter)
 // app.use('/SensorData',generalRouter)
 app.use('/auth',authRouter)
-// app.use('/PlantPrediction',plantRouter)
+app.use('/PlantPrediction',plantRouter)
 app.use('/report',reportRouter)
 app.use('/', scrapeRouter);
 
