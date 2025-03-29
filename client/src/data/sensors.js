f79e9efe2972a302a407f744297c341c909e5826
SiddharthJiyani <sidjiyani2003@gmail.com>
2025-03-29 19:13:37 +0530

    Add farm map

---
+++ client/src/data/sensors.js
@@ -0,0 +1,13 @@
+// src/data/sensors.js
+export const sensors = [
+    { 
+      id: 1, 
+      name: 'Soil Moisture Sensor 1', 
+      position: [YOUR_LATITUDE, YOUR_LONGITUDE], 
+      type: 'moisture',
+      value: 45,
+      status: 'normal',
+      color: '#3388ff' 
+    },
+    // Add all your sensors with their coordinates
+  ];
